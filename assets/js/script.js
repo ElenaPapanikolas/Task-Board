e116eab75a984d87692bb26d724b966a3b4dfbea
Elena Papanikolas <epapanikolas@hotmail.com>
2024-05-15 19:47:05 -0600

    Added event listener to add task button, created function to handle adding a task.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,27 @@
+
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-//
-if (!taskList) {
-    taskList = [];
-}
- function storeTask(newTask) {
-    taskList.push(newTask);
-    localStorage.setItem("tasks", JSON.stringify(taskList));
- }
-
- function getTask() {
+// function to get information
+function readFromStorage() {
+    let taskList = JSON.parse(localStorage.getItem('tasks'));
+    if (!taskList) {
+        taskList = [];
+    }
     return taskList;
- }
- 
+}
+
+// function to save information
+function saveToStorage(taskList) {
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+}
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return crypto.randomUUID();
 }
 
 // Todo: create a function to create a task card
@@ -32,8 +35,33 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
+    event.preventDefault();
 
+    const taskList = readFromStorage();
+    const taskTitle = $("#task-title");
+    const taskDueDate = $("#task-due-date");
+    const taskDescription = $("#task-description");
+
+    const newTask = {
+        id: generateTaskId(),
+        title: taskTitle.val(),
+        dueDate: taskDueDate.val(),
+        description: taskDescription.val(),
+        status: 'to-do',
+    }
+
+    
+
+    taskList.push(newTask);
+    saveToStorage(taskList);
+    renderTaskList(newTask);
+    console.log(taskList);  // take this out eventually
+    //  clear task input form after each use
+    taskTitle.val('');
+    taskDueDate.val('');
+    taskDescription.val('');
+    
 }
 
 // Todo: create a function to handle deleting a task
@@ -48,14 +76,14 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    $("#taskbtn").on("click", handleAddTask);
 
 
 
 
 // datepicker
     $(function() {
-        $( "#task-due-date" ).datepicker({
+        $("#task-due-date").datepicker({
             changeMonth: true,
             changeYear: true
           });
